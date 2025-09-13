1ef0bdea80cd2666fa5251051c9b3c41b0d8a814
roumaissa <roumaissa8901@gmail.com>
2025-09-13 17:10:45 +0100

    fix my recipe service

--- mobile/services/recipes.js
+++ mobile/services/recipes.js
@@ -2,10 +2,28 @@ import axios from "axios";
 
 export const getRecipes = async () => {
   try {
-    const res = await axios.get("http://10.0.2.2:5001/api/recipes");
+    const res = await axios.get("http://192.168.100.9:5001/api/recipes");
     return res.data;
   } catch (err) {
     console.error("API error:", err);
     throw err;
+  }
+};
+
+export const getRecipeById = async (id) => {
+  try {
+    const allRecipes = await getRecipes();
+    const recipe = allRecipes.recipes.find(
+      (r) => r.id.toString() === id.toString()
+    );
+
+    if (!recipe) {
+      throw new Error("Recipe not found");
+    }
+
+    return recipe;
+  } catch (error) {
+    console.error("Error fetching recipe by ID:", error);
+    throw error;
   }
 };
