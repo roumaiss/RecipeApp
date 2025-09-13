2892bc10951a17732a99455358950ee8425d9920
roumaissa <roumaissa8901@gmail.com>
2025-09-13 22:48:08 +0100

    card heart for favorites

--- mobile/components/Card.jsx
+++ mobile/components/Card.jsx
@@ -1,13 +1,35 @@
-import { View, Text, StyleSheet, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 import { Image } from "expo-image";
 import CreamCake from "../constants/colors";
+import Entypo from "@expo/vector-icons/Entypo";
+import { useState } from "react";
+
 const { width } = Dimensions.get("window");
 const cardWidth = (width - 40) / 2;
+
 const Card = ({ recipe }) => {
   // const { width } = Dimensions.get("window");
   // const cardWidth = (width - 48) / 2;
+  const [click, setClick] = useState(false);
   return (
     <View style={CardStyle.container}>
+      <TouchableOpacity
+        onPress={() => setClick(!click)}
+        style={CardStyle.heart}
+      >
+        {click ? (
+          <Entypo name="heart" size={24} color="white" />
+        ) : (
+          <Entypo name="heart-outlined" size={24} color="white" />
+        )}
+      </TouchableOpacity>
+
       <Image
         source={{ uri: `${process.env.EXPO_PUBLIC_BASE_URL}${recipe.image}` }}
         style={CardStyle.image}
@@ -23,7 +45,7 @@ const CardStyle = StyleSheet.create({
   container: {
     flex: 1,
     height: 240,
-    width:cardWidth,
+    width: cardWidth,
     padding: 12,
     borderRadius: 30,
     backgroundColor: CreamCake.background,
@@ -35,6 +57,7 @@ const CardStyle = StyleSheet.create({
     shadowOpacity: 0.55, // 55% opacity
     shadowRadius: 8, // Blur value
     elevation: 8,
+    position: "relative",
   },
   image: { alignSelf: "stretch", height: 120, borderRadius: 20 },
   title: {
@@ -47,6 +70,12 @@ const CardStyle = StyleSheet.create({
     fontWeight: "600",
     color: CreamCake.text,
     fontSize: 12,
+  },
+  heart: {
+    position: "absolute",
+    zIndex: 10,
+    top: 15,
+    right: 17,
   },
 });
 export default Card;
