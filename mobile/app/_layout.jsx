4f290f8c0cd6a8baeae4303506a2dae28f1a8a04
roumaissa <roumaissa8901@gmail.com>
2025-09-19 18:43:36 +0100

    add tanstack query

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -3,13 +3,17 @@ import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import Toast from "react-native-toast-message";
 import SafeScreen from "../components/SafeScreen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 export default function RootLayout() {
+  const queryClient = new QueryClient();
   return (
-    <ClerkProvider tokenCache={tokenCache}>
-      <SafeScreen>
-        <Slot />
-        <Toast position="top" topOffset={50} />
-      </SafeScreen>
-    </ClerkProvider>
+    <QueryClientProvider client={queryClient}>
+      <ClerkProvider tokenCache={tokenCache}>
+        <SafeScreen>
+          <Slot />
+          <Toast position="top" topOffset={50} />
+        </SafeScreen>
+      </ClerkProvider>
+    </QueryClientProvider>
   );
 }
