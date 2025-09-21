a7cfa284a99bee6cc9bd1a7e407ddd5ccda02b32
roumaissa <roumaissa8901@gmail.com>
2025-09-21 16:16:10 +0100

    add the screen header remove in the mobile recipe page

--- mobile/app/recipe/[id].jsx
+++ mobile/app/recipe/[id].jsx
@@ -1,9 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
 
 const Details = () => {
   return (
     <View>
+      <Stack.Screen
+        options={{
+          headerShown:false
+        }}
+      />
       <Text>Details</Text>
     </View>
   )
